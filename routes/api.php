881f50a525dfc8d50b5b7a5c483c83e9b8b6b665
MEBARKI Mehdi <mehdi-mebarki@live.fr>
2025-03-06 18:30:13 +0100

    add prestation api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\PrestationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,4 +14,8 @@ Route::prefix('auth')
     ->group(function () {
         Route::post('/login', [AuthController::class, 'login'])->name('login');
         Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('prestations', PrestationController::class);
 });
